237d8099a62681c128d10abc5d05154ff845dfcf
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-04-26 14:58:53 +0000

    Bump version: 0.0.3 → 0.0.4

--- src/comps_sif_constructor/version.py
+++ src/comps_sif_constructor/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ["__version__", "__versiondate__", "__license__"]
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __versiondate__ = '2025-04-26'
 __license__ = "Apache-2.0"
