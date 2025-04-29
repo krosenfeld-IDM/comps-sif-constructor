720bca520f255b39ace6fae271e38c60606f09a1
Katherine <krosenf@gmail.com>
2025-04-29 21:44:26 +0000

    switch to click

--- src/comps_sif_constructor/create_sif.py
+++ src/comps_sif_constructor/create_sif.py
@@ -6,42 +6,23 @@ Usage:
     comps_sif_constructor -d <path_to_definition_file> -o <output_id> -i <image_name> -w <work_item_name> [-r <requirements_file>]
 """
 
-import argparse
+import click
 from idmtools.core.platform_factory import Platform
 from idmtools_platform_comps.utils.singularity_build import SingularityBuildWorkItem
 from idmtools.assets.file_list import FileList
 import traceback
 import sys
 
-def main(file='lolcow.def'):
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--definition_file", "-d", type=str, help="Path to the Singularity definition file", default=file
-    )
-    parser.add_argument(
-        "--output_id", "-o", type=str, help="(optional) Name out Asset id file", default="sif.id"
-    )
-    parser.add_argument(
-        "--image_name", "-i", type=str, help="(optional) Name of the Singularity image file", default="lolcow_0.0.1.sif"
-    )
-    parser.add_argument(
-        "--work_item_name", "-w",
-        type=str,
-        help="(optional) Name of the work item",
-        default="Singularity Build",
-    )
-    parser.add_argument("--requirements", "-r", type=str, help="(optional) Path to the requirements file", default=None)
-    args = parser.parse_args()
-
+def create_sif(definition_file, output_id, image_name, work_item_name, requirements):
     kwargs = {}
-    if args.requirements is not None:
-        kwargs["asset_files"] = FileList(files_in_root=[args.requirements])
+    if requirements is not None:
+        kwargs["asset_files"] = FileList(files_in_root=[requirements])
 
     platform = Platform("CALCULON")
     sbi = SingularityBuildWorkItem(
-        name=args.work_item_name,
-        definition_file=args.definition_file,
-        image_name=args.image_name,
+        name=work_item_name,
+        definition_file=definition_file,
+        image_name=image_name,
         **kwargs
     )
     sbi.tags = dict(my_key="my_value")
@@ -53,7 +34,16 @@ def main(file='lolcow.def'):
         sys.exit(1)
 
     if sbi.succeeded:
-        sbi.asset_collection.to_id_file(args.output_id)
+        sbi.asset_collection.to_id_file(output_id)
+
+@click.command()
+@click.option("--definition_file", "-d", type=str, help="Path to the Singularity definition file", default="lolcow.def")
+@click.option("--output_id", "-o", type=str, help="(optional) Name out Asset id file", default="sif.id")
+@click.option("--image_name", "-i", type=str, help="(optional) Name of the Singularity image file", default="lolcow_0.0.1.sif")
+@click.option("--work_item_name", "-w", type=str, help="(optional) Name of the work item", default="Singularity Build")
+@click.option("--requirements", "-r", type=str, help="(optional) Path to the requirements file", default=None)
+def main(definition_file, output_id, image_name, work_item_name, requirements):
+    create_sif(definition_file, output_id, image_name, work_item_name, requirements)
 
 if __name__ == "__main__":
     main()
